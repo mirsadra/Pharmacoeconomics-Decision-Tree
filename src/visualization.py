19cd44e507c5ce3afa527f2e0a3fb3f9f1a87990
Mirsadra Molaei <miirsadra@gmail.com>
2024-09-14 23:05:39 +0330

    plot decision tree

--- src/visualization.py
+++ src/visualization.py
@@ -1,1 +1,24 @@
+# visualization.py
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def plot_decision_tree(decision_node):
+    """
+    Plots the decision tree.
+    """
+    G = nx.DiGraph()
+
+    def add_nodes(node, parent=None):
+        G.add_node(node.name)
+        if parent:
+            G.add_edge(parent.name, node.name)
+        for branch in getattr(node, 'branches', []):
+            add_nodes(branch, node)
+        for next_node in getattr(node, 'next_nodes', []):
+            add_nodes(next_node, node)
+
+    add_nodes(decision_node)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, arrows=True)
+    plt.show()
